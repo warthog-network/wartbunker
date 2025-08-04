f63e7226ade841bb63d4aeb7a84d6d57693b5241
Jax <lucidbrawler@hatchet.com>
2025-08-04 13:28:41 -0400

    maybe fixed center issue in prod

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 
 import WebWallet from './pages/Wallet.jsx'
 import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     
